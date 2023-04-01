e8c5abbfb6be5ab100f1bf5909cbbd9842440d6e
Obinna Igwe <igwenus619@gmail.com>
2023-04-01 08:23:55 +0100

    Add transformers

--- lib/src/resource.dart
+++ lib/src/resource.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:meta/meta.dart';
 
 /// A wrapper that encapsulates either a value or a throwable
@@ -12,19 +10,45 @@ abstract class Resource<S, F extends Object> {
   /// Returns an instance that encapsulates a throwable as a failure.
   factory Resource.failure(F throwable) = Failure<S, F>;
 
+  //===========================================================================
   // Shorthand for try/catch
   //===========================================================================
 
-  /// Runs [function] and returns an instance that encapsulates a value
+  /// Runs [function] and returns a resource that encapsulates a value
   /// of type [V]
   ///
-  /// If an error occurred while running [function], it returns an instance
-  /// that encapsulates the error as type [T].
+  /// If an error occurs while running [function], it returns a resource
+  /// that encapsulates a throwable of type [T].
   ///
   /// [onFailure] can be used to map/process
   /// the error and stacktrace to any throwable of type [T] before
   /// the failure instance is returned
-  static Future<Resource<V, T>> guardRun<V, T extends Object>({
+  static Resource<V, T> guardSync<V, T extends Object>({
+    required V Function() function,
+    T Function(Object throwable, StackTrace stackTrace)? onFailure,
+  }) {
+    try {
+      return Resource.success(function.call());
+    } catch (err, stacktrace) {
+      if (onFailure != null) {
+        final throwable = onFailure.call(err, stacktrace);
+        return Resource.failure(throwable);
+      }
+      return Resource.failure(err as T);
+    }
+  }
+
+  /// Runs the async [function] and returns a Future that encapsulates
+  /// a resource with a value of type [V]
+  ///
+  /// If an error occurs while running [function], it returns
+  /// a Future that encapsulates a resource with a throwable
+  /// of type [T].
+  ///
+  /// [onFailure] can be used to map/process
+  /// the error and stacktrace to any throwable of type [T] before
+  /// the failure instance is returned
+  static Future<Resource<V, T>> guardAsync<V, T extends Object>({
     required Future<V> Function() function,
     T Function(Object throwable, StackTrace stackTrace)? onFailure,
   }) async {
@@ -39,7 +63,16 @@ abstract class Resource<S, F extends Object> {
     }
   }
 
-  /// Like guardRun, but for streams
+  /// Runs the [function] and emits a Stream that encapsulates
+  /// a resource with a value of type [V]
+  ///
+  /// If an error occurs while running [function], it emits
+  /// a Stream that encapsulates a resource that encapsulates a throwable
+  /// of type [T].
+  ///
+  /// [onFailure] can be used to map/process
+  /// the error and stacktrace to any throwable of type [T] before
+  /// the failure instance is returned
   static Stream<Resource<V, T>> guardEmit<V, T extends Object>({
     required Stream<V> Function() function,
     T Function(Object throwable, StackTrace stackTrace)? onFailure,
@@ -57,6 +90,7 @@ abstract class Resource<S, F extends Object> {
     }
   }
 
+  //===========================================================================
   // Discovering the status
   //===========================================================================
 
@@ -70,6 +104,7 @@ abstract class Resource<S, F extends Object> {
   ///In this case [isSuccess] returns false.
   bool get isFailure => this is Failure<S, F>;
 
+  //===========================================================================
   // Getting values and exceptions
   //===========================================================================
 
@@ -100,6 +135,7 @@ abstract class Resource<S, F extends Object> {
   /// encapsulated throwable
   S valueOrThrow();
 
+  //===========================================================================
   // Handling events
   //===========================================================================
 
@@ -116,16 +152,71 @@ abstract class Resource<S, F extends Object> {
   /// Runs [onSuccess] on the encapsulated value if
   /// this instance represents a success and returns the original instance
   ///  unchanged.
-  Resource<S, F> whenSuccess({required void Function(S value) onSuccess});
+  Resource<S, F> whenSuccess({
+    required void Function(S value) onSuccess,
+  });
 
   /// Runs [onFailure] on the encapsulated throwable if
   /// this instance represents failure and returns the original instance
   /// unchanged.
-  Resource<S, F> whenFailure({required void Function(F throwable) onFailure});
+  Resource<S, F> whenFailure({
+    required void Function(F throwable) onFailure,
+  });
 
-// Transforming values and exceptions
-//===========================================================================
-// Coming soon...
+  //==========================================================================
+  // Transforming values and exceptions
+  //==========================================================================
+
+  /// Returns the encapsulated result of the given [transformer] applied
+  /// to the encapsulated value if this instance represents success or
+  /// the original encapsulated throwable if it is failure.
+  Resource<T, F> map<T>(
+    T Function(S value) transformer,
+  );
+
+  /// Returns the result of the given [transformer] applied
+  /// to the encapsulated value if this instance represents a success or
+  /// the original encapsulated throwable if it is failure.
+  ///
+  /// If an error occurs while running the given [transformer],
+  /// it returns a failure.
+  Resource<T, F> guardMap<T>(
+    T Function(S value) transformer,
+  );
+
+  /// Returns the encapsulated result of the given [transformer] applied
+  /// to the encapsulated throwable if this instance represents a failure or
+  /// the original encapsulated value if it is a success.
+  Resource<S, T> mapFailure<T extends Object>(
+    T Function(F throwable) transformer,
+  );
+
+  /// Returns the encapsulated result of the given [transformer] applied
+  /// to the encapsulated throwable if this instance represents a failure or
+  /// the original encapsulated value if it is a success.
+  ///
+  /// If an error occurs while running the given [transformer],
+  /// it returns a failure.
+  Resource<S, T> guardMapFailure<T extends Object>(
+    T Function(F throwable) transformer,
+  );
+
+  /// Returns a success after applying the given [transformer]
+  /// to the encapsulated throwable if this instance represents a failure or
+  /// the original encapsulated value if it is a success.
+  Resource<S, F> recover(
+    S Function(F throwable) transformer,
+  );
+
+  /// Returns a success after applying the given [transformer]
+  /// to the encapsulated throwable if this instance represents a failure or
+  /// the original encapsulated value if it is a success.
+  ///
+  /// If an error occurs while running the given [transformer],
+  /// it returns a failure.
+  Resource<S, F> guardRecover(
+    S Function(F throwable) transformer,
+  );
 }
 
 /// A wrapper that encapsulates a value
@@ -172,6 +263,50 @@ class Success<S, F extends Object> extends Resource<S, F> {
     required void Function(F throwable) onFailure,
   }) {
     return this;
+  }
+
+  @override
+  Resource<T, F> map<T>(T Function(S value) transformer) {
+    final transformedValue = transformer(_value);
+    return Resource.success(transformedValue);
+  }
+
+  @override
+  Resource<T, F> guardMap<T>(T Function(S value) transformer) {
+    return Resource.guardSync(
+      function: () {
+        final transformedValue = transformer(_value);
+        return transformedValue;
+      },
+    );
+  }
+
+  @override
+  Resource<S, T> mapFailure<T extends Object>(
+    T Function(F throwable) transformer,
+  ) {
+    return Resource.success(_value);
+  }
+
+  @override
+  Resource<S, T> guardMapFailure<T extends Object>(
+    T Function(F throwable) transformer,
+  ) {
+    return Resource.success(_value);
+  }
+
+  @override
+  Resource<S, F> recover(
+    S Function(F throwable) transformer,
+  ) {
+    return Success(_value);
+  }
+
+  @override
+  Resource<S, F> guardRecover(
+    S Function(F throwable) transformer,
+  ) {
+    return Success(_value);
   }
 
   @override
@@ -240,6 +375,64 @@ class Failure<S, F extends Object> extends Resource<S, F> {
   }) {
     onFailure(_throwable);
     return this;
+  }
+
+  @override
+  Resource<T, F> map<T>(T Function(S value) transformer) =>
+      Resource.failure(_throwable);
+
+  @override
+  Resource<T, F> guardMap<T>(T Function(S value) transformer) =>
+      Resource.failure(_throwable);
+
+  @override
+  Resource<S, T> mapFailure<T extends Object>(
+    T Function(F throwable) transformer,
+  ) {
+    final transformedThrowable = transformer(_throwable);
+    return Resource.failure(transformedThrowable);
+  }
+
+  @override
+  Resource<S, T> guardMapFailure<T extends Object>(
+    T Function(F throwable) transformer,
+  ) {
+    return Resource.guardSync(
+      function: () {
+        final transformedThrowable = transformer(_throwable);
+
+        if (transformedThrowable is Error) {
+          throw transformedThrowable;
+        }
+        if (transformedThrowable is Exception) {
+          throw _throwable as Exception;
+        }
+
+        throw Exception(
+          'You can only throw an error or an exception',
+        );
+      },
+    );
+  }
+
+  @override
+  Resource<S, F> recover(
+    S Function(F throwable) transformer,
+  ) {
+    final transformedValue = transformer(_throwable);
+    return Success(transformedValue);
+  }
+
+  @override
+  Resource<S, F> guardRecover(
+    S Function(F throwable) transformer,
+  ) {
+    return Resource.guardSync(
+      function: () {
+        final transformedValue = transformer(_throwable);
+        return transformedValue;
+      },
+    );
   }
 
   @override
